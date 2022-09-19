c7a21345b1bd2186028997c677f978d8e2b3782f
wniebelski <wniebelski@gmail.com>
2022-09-19 18:38:18 +0200

    rozwiązanie

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let firstNumber = 10;
+let secondNumber = 20;
+function adding (any, any){
+let resoult = firstNumber + secondNumber
+return resoult}
+console.log (adding (firstNumber, secondNumber))
