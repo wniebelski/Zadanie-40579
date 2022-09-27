5f21cb91f7a2b191f927e4d944a55318212c77e4
wniebelski <wniebelski@gmail.com>
2022-09-27 07:53:10 +0200

    korekta

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let firstNumber = 10;
 let secondNumber = 20;
-function adding (Num1, Num2){
-let resoult = firstNumber + secondNumber
-return resoult}
-console.log (adding (10, 20))
+
+function adding (num1, num2){
+     let resoult = num1 + num2
+     return resoult};
+
+console.log (adding (firstNumber, secondNumber));
